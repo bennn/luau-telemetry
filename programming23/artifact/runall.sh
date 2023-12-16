409e5039158a7c070d7b7dcb175f20c8aa543a24
Ben Greenman <benjaminlgreenman@gmail.com>
2023-12-16 10:40:36 -0500

    runall: reorder, add time checkins

--- programming23/artifact/runall.sh
+++ programming23/artifact/runall.sh
@@ -3,23 +3,41 @@ echo "=== before"
 ls out/
 rm -f out/*.*
 echo "=== after"
+echo "=== rebuilding ..."
 
+echo "TIME $(date +%F_%T)"
 PLTSTDERR="error info@luau" racket code/prepare.rkt
+echo "TIME $(date +%F_%T)"
 PLTSTDERR="error info@luau" racket code/prepare.rkt --mode size-distro
+echo "TIME $(date +%F_%T)"
 PLTSTDERR="error info@luau" racket code/prepare.rkt --mode modswitch
+echo "TIME $(date +%F_%T)"
 PLTSTDERR="error info@luau" racket code/prepare.rkt --mode divide-sessions
+echo "TIME $(date +%F_%T)"
 PLTSTDERR="error info@luau" racket code/prepare.rkt --mode session-query
+echo "TIME $(date +%F_%T)"
 PLTSTDERR="error info@luau" racket code/prepare.rkt --mode session-fold
+echo "TIME $(date +%F_%T)"
 PLTSTDERR="error info@luau" racket code/prepare.rkt --mode count-te-editrange 
+echo "TIME $(date +%F_%T)"
 PLTSTDERR="error info@luau" racket code/prepare.rkt --mode aggregate-te
 
+echo "TIME $(date +%F_%T)"
 racket code/row-distribution.rkt
-racket code/size-distribution.rkt
+echo "TIME $(date +%F_%T)"
 racket code/sdupdate.rkt
+echo "TIME $(date +%F_%T)"
+racket code/size-distribution.rkt
+echo "TIME $(date +%F_%T)"
 racket code/downgrade-count.rkt
+echo "TIME $(date +%F_%T)"
 racket code/error-by-mode.rkt
+echo "TIME $(date +%F_%T)"
 racket code/type-error-survival.rkt
+echo "TIME $(date +%F_%T)"
 racket code/type-error-count.rkt
+echo "TIME $(date +%F_%T)"
 racket code/error-count.rkt
+echo "TIME $(date +%F_%T)"
 
-echo "DONE BUILDING, next step = compile pdf outside docker"
+echo "\nDONE BUILDING, next step = compile pdf outside docker"
